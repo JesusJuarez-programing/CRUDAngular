089729e1ae46bc4b3c63d22efb92be7687c6432d
JesusJuarez-programing <juarez.devora.ja@gmail.com>
2020-07-25 12:43:11 -0700

    Read

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -31,4 +31,24 @@ export class HeroesService {
     delete heroeTemp.id;
     return this.http.put(`${this.url}/heroes/${heroe.id}.json`, heroeTemp);
   }
+
+  getHeroes(){
+    return this.http.get(`${this.url}/heroes.json`).pipe(
+      map( this.crearArreglo )
+    );
+  }
+
+  private crearArreglo( heroesObj: object ){
+    const heroes: HeroeModel[] = [];
+    console.log(heroesObj);
+
+    if (heroesObj === null) { return []; }
+
+    Object.keys( heroesObj ).forEach( key => {
+      const heroe: HeroeModel = heroesObj[key];
+      heroe.id = key;
+      heroes.push(heroe);
+    });
+    return heroes;
+  }
 }
